b11fd8abbbf4c0c17574750a4ebd01f8831ce66e
Paul Roujansky <paul@roujansky.eu>
2018-04-22 23:14:43 +0200

    Draft causes/type of death

--- sql_queries/cohort.sql
+++ sql_queries/cohort.sql
@@ -2,10 +2,9 @@
 -- INCLUSION CRITERIA: --
 -------------------------
 
-	-- AGE > 15
-	-----------
+-- AGE > 15
+-----------
 
-query = """
 WITH person_age AS (
     SELECT p.person_id, (v.visit_start_date - p.birth_datetime)/365.25 age
     FROM person p
@@ -17,19 +16,16 @@ WHERE age > 15            -- including persons older than 15
 AND age < 150             -- removing any outliers
 ORDER BY age
 ;
-"""
-
-# person_age = pd.read_sql_query(query, conn)
-
 
 -- ICU Units (tbc)
 ------------------
-query = """
-SELECT care_site_id, care_site_name, place_of_service_source_value
-FROM care_site
-WHERE LOWER(place_of_service_source_value) LIKE '%intensive%'     -- selecting ICUs
-AND LOWER(place_of_service_source_value) NOT LIKE '%neonatal%'    -- removing Neonatal ICUs
-LIMIT 100;
-"""
 
-# ICU = pd.read_sql_query(query, conn)
+WITH icu AS (
+    SELECT care_site_id, care_site_name, place_of_service_source_value
+    FROM care_site
+    WHERE LOWER(place_of_service_source_value) LIKE '%intensive%'     -- selecting ICUs
+    AND LOWER(place_of_service_source_value) NOT LIKE '%neonatal%'    -- removing Neonatal ICUs
+    )
+SELECT vd.person_id, icu.place_of_service_source_value
+FROM visit_detail vd
+JOIN icu ON icu.care_site_id = vd.care_site_id
