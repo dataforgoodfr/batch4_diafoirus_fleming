098041cdd48ded1d5c10f45ddcc2c301f02c6e3c
Paul Roujansky <paul@roujansky.eu>
2018-05-13 17:37:20 +0200

    Cleaned data loading and merging.

--- fleming_lib/dataset.py
+++ fleming_lib/dataset.py
@@ -3,11 +3,14 @@ import time
 
 import pandas as pd
 
-from .metrics import add_age, add_rolling_avg, add_target
-from .utils import to_categorical, to_onehot
+from fleming_lib.metrics import add_age, add_rolling_avg, add_target
+from fleming_lib.utils import (add_categories, add_missing_columns,
+                               check_length, convert_frac, to_categorical,
+                               to_numeric, to_onehot)
 
 
-def create_dataset(conn, list_patients, verbose=False):
+def create_dataset(conn, list_patients, n_patients_per_batch=10,
+                   verbose=False):
     """Create list of dataset given a list of patients.
 
     Parameters
@@ -16,13 +19,16 @@ def create_dataset(conn, list_patients, verbose=False):
         Active connection to the OMOP database.
     list_patients : list of int
         List of patients ID.
-    verbose : bool
+    n_patients_per_batch : int (default=10)
+        Number of patients to sequentially load data for, in order not to cause
+        timeout if the query is too long to process by the server.
+    verbose : bool (default=False)
         Verbosity level.
 
     Returns
     -------
-    frame : list of pd.DataFrame
-        List of datasets, each corresponding to a patient.
+    dataset : pd.DataFrame
+        Dataset containing all data associated to each patient.
 
     """
     t0 = time.time()
@@ -33,7 +39,8 @@ def create_dataset(conn, list_patients, verbose=False):
 
     n_patients = len(list_patients)
 
-    # Meta data
+    # Extract meta data
+    # -----------------
     if verbose:
         msg = 'Extracting meta data...'
         delta_t = str(int(time.time() - t0)) + ' s'
@@ -49,26 +56,68 @@ def create_dataset(conn, list_patients, verbose=False):
 
     meta = pd.read_sql_query(query, conn)
 
+    # Dictionary containing unique categories for each categorical variable
+    categories = dict()
+
     # Convert categorical variable to 'categorical' type
     categorical_variables = ['gender', 'race']
-    meta = to_categorical(meta, categorical_variables)
-    meta = to_onehot(meta, categorical_variables)
-    # One-hot column names
-    meta_names = meta.columns
+    # Extracting categories for each categorical variable
+    categories = add_categories(categories, meta, categorical_variables)
 
-    for i, patient in enumerate(list_patients):
+    meta = to_categorical(meta, categorical_variables, categories)
+    meta = to_onehot(meta, categorical_variables)
+
+    # Extract unique measurements values from categorical variables
+    # (here 'Heart rate rhythm')
+    query = """
+        select
+            distinct m.measurement_concept_name, m.value_source_value
+        from
+            measurement m
+        where
+            m.measurement_concept_id in
+            (3022318   -- heart_rhythm
+            )
+        order by
+            m.measurement_concept_name, m.value_source_value
+        ;"""
+
+    unique_categ_values = pd.read_sql_query(query, conn)
+    categorical_variables = ['Heart rate rhythm']
+    # Adding categories of each categorical variables to dict 'categories'
+    for var in categorical_variables:
+        tmp = unique_categ_values.loc[
+            unique_categ_values.measurement_concept_name == var]
+        tmp.drop('measurement_concept_name', axis=1, inplace=True)
+        tmp.rename(index=str, columns={'value_source_value': var},
+                   inplace=True)
+        categories = add_categories(categories, tmp, var)
+
+    # Create sublist of patients (batch)
+    sublists_patients = [list_patients[i: i+n_patients_per_batch]
+                         for i in range(0, n_patients, n_patients_per_batch)]
+    n_sublists = len(sublists_patients)
+
+    # Extracting data for each patient
+    for i, sublist_patients in enumerate(sublists_patients):
         if verbose:
-            base_msg = 'Patient {} [{}/{}]'.format(patient, i+1, n_patients)
+            base_msg = 'Batch {}/{}'.format(i+1, n_sublists)
             msg = base_msg
             delta_t = str(int(time.time() - t0)) + ' s'
             print('{:100s} [{:10s}]'.format(msg, delta_t), end='\r')
 
-        # Measures
+        # Extract measures
+        # ----------------
         if verbose:
             add_msg = 'Extracting measures...'
             msg = base_msg + ' - ' + add_msg
             delta_t = str(int(time.time() - t0)) + ' s'
             print('{:100s} [{:10s}]'.format(msg, delta_t), end='\r')
+
+        if len(sublist_patients) == 1:
+            match_person = "m.person_id = {}".format(sublist_patients[0])
+        else:
+            match_person = "m.person_id in {}".format(tuple(sublist_patients))
 
         query = """
         select
@@ -93,11 +142,14 @@ def create_dataset(conn, list_patients, verbose=False):
              3020716,   -- fio2
              3032652    -- glasgow coma scale
             )
-        and m.person_id = {}
+        and {}
         order by measurement_datetime
-            ;""".format(patient)
+            ;""".format(match_person)
 
         df = pd.read_sql_query(query, conn)
+
+        # Check if data is empty for a patient
+        check_length(df)
 
         if verbose:
             add_msg = 'Formatting data...'
@@ -108,57 +160,75 @@ def create_dataset(conn, list_patients, verbose=False):
         df['death_datetime'] = pd.to_datetime(df['death_datetime'])
         df['measurement_datetime'] = pd.to_datetime(df['measurement_datetime'])
 
-        df = add_target(df)
+        # Add target: 'death' status
+        df = df.groupby('person_id').apply(add_target)
 
+        # Convert to timeseries matrix
         df = df.pivot_table(
             index=['measurement_datetime', 'target', 'person_id'],
-            columns='measurement_concept_name',
-            values='value_source_value',
+            columns='measurement_concept_name', values='value_source_value',
             aggfunc='first')
         df.reset_index(inplace=True)
         df.columns.name = None
 
+        # Convert types
+        # -------------
         # Convert to numerical
         numerical_variables = [
-            'BP diastolic', 'BP systolic', 'Body temperature', 'Heart rate',
-            'Mean blood pressure', 'Oxygen saturation in Arterial blood',
-            'Respiratory rate']
-        df[numerical_variables] = df[numerical_variables].apply(
-            pd.to_numeric, errors='ignore')
+            'BP diastolic', 'BP systolic','Body temperature', 'Heart rate',
+            'Mean blood pressure', 'Glasgow coma scale',
+            'Oxygen concentration breathed',
+            'Mean pressure Respiratory system airway Calculated',
+            'Oxygen saturation in Arterial blood', 'Respiratory rate']
 
-        # Convert to categorical and one-hot encode it
+        df = add_missing_columns(df, numerical_variables)
+
+        df = convert_frac(df, numerical_variables)
+        df = to_numeric(df, numerical_variables)
+
+        # Convert to categorical and one-hot encode
         categorical_variables = ['Heart rate rhythm']
-        df = to_categorical(df, categorical_variables)
+
+        df = add_missing_columns(df, categorical_variables)
+
+        df = to_categorical(df, categorical_variables, categories)
         df = to_onehot(df, categorical_variables)
 
         # Add meta data to measures
+        # -------------------------
         if verbose:
             add_msg = 'Adding meta data...'
             msg = base_msg + ' - ' + add_msg
             delta_t = str(int(time.time() - t0)) + ' s'
             print('{:100s} [{:10s}]'.format(msg, delta_t), end='\r')
 
-        meta_idx = (meta['person_id'] == patient)
-        for meta_name in meta_names:
-            df[meta_name] = meta[meta_idx][meta_name].values.squeeze()
-
-        df = add_age(df, round_to_dec=1)
+        df = pd.merge(df, meta, how='inner', on='person_id')
 
         # Add additional features
+        # -----------------------
         if verbose:
             add_msg = 'Adding additional features...'
             msg = base_msg + ' - ' + add_msg
             delta_t = str(int(time.time() - t0)) + ' s'
             print('{:100s} [{:10s}]'.format(msg, delta_t), end='\r')
 
-        df = add_rolling_avg(df, 'Respiratory rate', window=2)
+        # - age
+        df = df.groupby('person_id').apply(add_age, round_to_dec=1)
+        # - 2h rolling average respiratory rate
+        df = df.groupby('person_id').apply(
+            add_rolling_avg, column='Respiratory rate', window=2)
 
         frame.append(df)
 
         if verbose:
-            msg = 'Patient {} done.'.format(patient)
+            add_msg = 'Done'
+            msg = base_msg + ' - ' + add_msg
             delta_t = str(int(time.time() - t0)) + ' s'
             print('{:100s} [{:10s}]'.format(msg, delta_t), end='\r')
             print('')
 
-    return frame
+    # Concat dataframes
+    dataset = pd.concat(frame)
+    dataset = dataset.reindex_axis(frame[0].columns, axis=1)
+
+    return dataset
--- fleming_lib/utils.py
+++ fleming_lib/utils.py
@@ -1,16 +1,19 @@
 """Utils functions."""
+import warnings
+
+import numpy as np
 import pandas as pd
 
 
-def to_categorical(df, variables=[]):
-    """Convert variables to categorical type.
+def to_numeric(df, variables=[]):
+    """Convert variables to numerical (int or float) type.
 
     Parameters
     ----------
     df : pd.DataFrame
         Dataframe.
     variables : list of str
-        List of variables to convert to categorical.
+        List of variables to convert to numerical.
 
     Returns
     -------
@@ -18,8 +21,61 @@ def to_categorical(df, variables=[]):
         Dataframe with converted variables.
 
     """
+    # convert to list if one element
+    if isinstance(variables, str):
+        variables = [variables]
+    # check variables
+    _check_variables(df, variables)
+    # convert to numerical
     for var in variables:
+        df[var] = df[var].apply(pd.to_numeric, errors='ignore')
+
+    return df
+
+
+def to_categorical(df, variables=[], categories=None):
+    """Convert variables to categorical type.
+
+    Missing values (NaN) are filled with 'NaN' category.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe.
+    variables : list of str
+        List of variables to convert to categorical.
+    categories : dict of pd.core.indexes.base.Index | None
+        If provided, a set of categories to apply to each categorical variable.
+        The name fo the corresponding variable should be passed as the key of
+        dict. If no cartegories are provided, default ones are kept.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        Dataframe with converted variables.
+
+    """
+    # convert to list if one element
+    if isinstance(variables, str):
+        variables = [variables]
+    # check variables
+    _check_variables(df, variables)
+    # convert to categorical
+    for var in variables:
+        # df[var] = df[var].apply(pd.Categorical)
         df[var] = df[var].astype('category')
+        # convert missing values (NaN) to category 'NaN'
+        if 'Nan' not in df[var].cat.categories:
+            df[var] = df[var].cat.add_categories('NaN')
+        df[var].fillna('NaN', inplace=True)
+
+    if categories:
+        if not isinstance(categories, dict):
+            raise ValueError('`categories` should be a dict.')
+        # set categories of each categorical variable
+        for var in variables:
+            if var in categories:
+                df[var] = df[var].cat.set_categories(categories[var])
 
     return df
 
@@ -40,7 +96,9 @@ def to_onehot(df, variables=[]):
         Dataframe with added one-hot encoded variables.
 
     """
-    # Check whether input variables are categorical
+    # check variables
+    _check_variables(df, variables)
+    # check whether input variables are categorical
     for var in variables:
         if df[var].dtype != 'category':
             raise ValueError(
@@ -57,7 +115,7 @@ def to_onehot(df, variables=[]):
     return df
 
 
-def convert_frac(df, column):
+def convert_frac(df, variables):
     """Convert fractions to numerical values.
 
     Sometimes numbers are represented as literal in the following format
@@ -68,28 +126,31 @@ def convert_frac(df, column):
     ----------
     df : pd.DataFrame
         Input dataframe.
-    column : str
-        Name of the column to convert to float.
+    variables : list of str
+        List of variables to convert to numerical
 
     Returns
     -------
     df : pd.DataFrame
-        Dataframe with added column.
+        Dataframe with converted data.
 
     """
-    if column not in df:
-        raise ValueError('`{}` not in dataframe.'.format(column))
+    # convert to list if one element
+    if isinstance(variables, str):
+        variables = [variables]
+    # check variables
+    _check_variables(df, variables)
 
     def frac_to_num(row):
         """Convert fraction to numerical."""
         try:
-            if hasattr(row[column], 'str'):
-                tmp = row[column].str.split('/')
+            if hasattr(row, 'str'):
+                tmp = row.str.split('/')
             else:
-                if isinstance(row[column], str):
-                    tmp = row[column].split('/')
+                if isinstance(row, str):
+                    tmp = row.split('/')
                 else:
-                    return row[column]
+                    return row
             if tmp is not None:
                 if len(tmp) == 2:
                     num, denum = tmp
@@ -103,11 +164,121 @@ def convert_frac(df, column):
                     tmp = float(tmp[0])
             return tmp
         except ValueError:
-            return row[column]
+            return row
 
-    df[column] = df.apply(lambda x: frac_to_num(x), axis=1)
-
-    # convert to numerical type
-    df[column] = df[column].apply(pd.to_numeric, errors='ignore')
+    for var in variables:
+        df[var] = df[var].apply(lambda x: frac_to_num(x))
+        # convert to numerical type
+        df[var] = df[var].apply(pd.to_numeric, errors='ignore')
 
     return df
+
+
+def add_categories(categories, df, variables):
+    """Convert fractions to numerical values.
+
+    Sometimes numbers are represented as literal in the following format
+    'N/M' where N and M are integers. What follows convert these literal
+    fractions to decimal floats.
+
+    Parameters
+    ----------
+    categories : dict of dict of pd.core.indexes.base.Index
+        Dictionary containing categories corresponding to each variable (
+        passed as a key).
+    df : pd.DataFrame
+        Input dataframe.
+    variables : list of str
+        List of variables to convert to numerical
+
+    Returns
+    -------
+    categories : dict of dict of pd.core.indexes.base.Index
+        Dictionary with added categories for the given input variables.
+
+    """
+    # convert to list if one element
+    if isinstance(variables, str):
+        variables = [variables]
+    # check variables
+    _check_variables(df, variables)
+
+    if not categories:  # create empty dict
+        categories = dict()
+    if not isinstance(categories, dict):
+            raise ValueError('`categories` should be a dict (even empty).')
+
+    # copy
+    tmp = df.copy(deep=True)
+    # convert to categorical
+    to_categorical(tmp, variables)
+
+    for var in variables:
+        # checking input type
+        if tmp[var].dtype != 'category':
+            raise ValueError(
+                'Variable {} is not categorical, cannot one-hotencode.'.format(
+                    var))
+        categ_values = tmp[var]
+        categ_values = categ_values.astype('category')
+        # checking already-existing conflict
+        if var in categories:
+            wrn = '`{}` already in dict `categories`: overwriting.'.format(var)
+            warnings.warn(wrn)
+        categories[var] = categ_values.cat.categories
+
+    return categories
+
+
+def add_missing_columns(df, variables):
+    """Add column if missing and fill it with NaN values.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input dataframe.
+    variables : list of str
+        List of variables to check.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        Dataframe with added missing columns.
+
+    """
+    # convert to list if one element
+    if isinstance(variables, str):
+        variables = [variables]
+    for var in variables:
+        if var not in df:
+            df[var] = np.nan
+            wrn = ('Colmun `{}` missing: adding it and filling with '
+                   'NaN.'.format(var))
+            warnings.warn(wrn)
+
+    return df
+
+
+def check_length(df):
+    """Check whether some patients have empty data.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input dataframe.
+
+    """
+    _check_variables(df, 'person_id')
+    data_lengths = df.groupby('person_id').apply(len).reset_index()
+    for index, row in data_lengths.iterrows():  # iter through patients
+        if row[0] == 0:  # check if empty
+            warnings.warn('No data for patient {}'.format(row.person_id))
+
+
+def _check_variables(df, variables):
+    """Check if `variables` are contained in input dataframe."""
+    if isinstance(variables, str):
+        variables = [variables]
+    for var in variables:
+        if var not in df:
+            raise ValueError('`{}` not in dataframe.'.format(var))
